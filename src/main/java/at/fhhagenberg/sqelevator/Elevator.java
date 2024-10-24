5a42b4c40fea361905bee65459ce8802eeeff8d1
FelixKillermann <felix.killermann@web.de>
2024-10-24 22:51:34 +0200

    24102024_Data_Model

---
+++ src/main/java/at/fhhagenberg/sqelevator/Elevator.java
@@ -0,0 +1,99 @@
+package at.fhhagenberg.sqelevator;
+
+import java.util.Dictionary;
+import java.util.List;
+import java.util.Map;
+
+public class Elevator {
+
+    private int commitedDirection;
+    private int doorState;
+    private int accelleration; // accel in ft/sec^2
+    private int speed; // speed in ft/sec
+    private Map<Integer, Boolean> buttons; // push state of button
+    private int location; // height of elevator from ground
+    private int weight;
+    private int capacity;
+    private Map<Integer, Boolean> serviceableFloors; // list of floors that the elevator can stop at
+    private int targetedFloor;
+    public Elevator(Map<Integer, Boolean> serviceableFloors, final int capacity)
+    {
+        this.commitedDirection = IElevator.ELEVATOR_DIRECTION_UNCOMMITTED;
+        this.doorState = IElevator.ELEVATOR_DOORS_CLOSED;
+        this.accelleration = 0;
+        this.speed = 0;
+        this.weight = 0;
+        this.targetedFloor = 0;
+
+        this.serviceableFloors = serviceableFloors;
+        this.capacity = capacity;
+
+        // set all buttons to false within the serviceable floor range
+        for(int id : this.serviceableFloors.keySet())
+        {
+            if(serviceableFloors.get(id) != null) {
+                buttons.put(id, false);
+            }else{
+                System.err.println("Serviceable floor state at " + id + " not specified");
+            }
+        }
+    }
+    public int getCommitedDirection() {
+        return this.commitedDirection;
+    }
+
+    public void setCommitedDirection(int commitedDirection) {
+        this.commitedDirection = commitedDirection;
+    }
+
+    public int getDoorState() {
+        return doorState;
+    }
+
+    public int getAccelleration() {
+        return accelleration;
+    }
+
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public Map<Integer, Boolean> getButtons() {
+        return buttons;
+    }
+
+    public int getLocation() {
+        return location;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public Map<Integer, Boolean> getServiceableFloors() {
+        return serviceableFloors;
+    }
+
+    public void setServiceableFloors(int floor, boolean service) {
+        this.serviceableFloors.put(floor, service);
+    }
+
+    public int getTargetedFloor() {
+        return targetedFloor;
+    }
+
+    public void setTargetedFloor(int targetedFloor) {
+        this.targetedFloor = targetedFloor;
+    }
+
+    // get data from mqtt and calculate new speed, acceleration etc
+    public void update()
+    {
+
+    }
+}
---
+++ src/main/java/at/fhhagenberg/sqelevator/ElevatorSystem.java
@@ -0,0 +1,190 @@
+package at.fhhagenberg.sqelevator;
+
+import java.rmi.RemoteException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class ElevatorSystem implements IElevator{
+
+    private final int lowestFloor;
+    private final int highestFloor;
+    private List<Elevator> elevators;
+    private final int floorHeight;
+    private boolean downButtonPress;
+    private boolean upButtonPress;
+    private long clockTick;
+
+
+    public ElevatorSystem(final int numElevator, final int lowestFloor, final int highestFloor, final int capacity, final int floorHeight)
+    {
+        this.lowestFloor = lowestFloor;
+        this.highestFloor = highestFloor;
+        elevators = new ArrayList<>();
+        this.floorHeight = floorHeight;
+        this.downButtonPress = false;
+        this.upButtonPress = false;
+
+        Map<Integer, Boolean> serviceableFloors = new HashMap<>();
+
+        for(int i = lowestFloor; i <= highestFloor; i++)
+        {
+            serviceableFloors.put(i, true);
+        }
+
+
+        for(int i = 0; i < numElevator; i++)
+        {
+            if(elevators != null)
+            {
+                elevators.add(new Elevator(serviceableFloors, capacity));
+            }
+        }
+    }
+
+    public void addElevator(Map<Integer, Boolean> serviceableFloors, int capacity)
+    {
+        elevators.add(new Elevator(serviceableFloors, capacity));
+    }
+
+    public void addElevator(int capacity)
+    {
+        Map<Integer, Boolean> serviceableFloors = new HashMap<>();
+
+        for(int i = lowestFloor; i <= highestFloor; i++)
+        {
+            serviceableFloors.put(i, true);
+        }
+
+        elevators.add(new Elevator(serviceableFloors, capacity));
+    }
+    public void addElevator()
+    {
+        Map<Integer, Boolean> serviceableFloors = new HashMap<>();
+
+        for(int i = lowestFloor; i <= highestFloor; i++)
+        {
+            serviceableFloors.put(i, true);
+        }
+
+        elevators.add(new Elevator(serviceableFloors, elevators.get(0).getCapacity()));
+    }
+
+    @Override
+    public int getCommittedDirection(int elevatorNumber) throws RemoteException {
+        return elevators.get(elevatorNumber).getCommitedDirection();
+    }
+
+    @Override
+    public int getElevatorAccel(int elevatorNumber) throws RemoteException {
+        return elevators.get(elevatorNumber).getAccelleration();
+    }
+
+    @Override
+    public boolean getElevatorButton(int elevatorNumber, int floor) throws RemoteException {
+
+        // check for service range of elevator
+        if(!elevators.get(elevatorNumber).getServiceableFloors().get(floor))
+        {
+            System.err.println("Floor " + floor + " not within elevator service range.");
+        }
+        return elevators.get(elevatorNumber).getButtons().get(floor);
+    }
+
+    @Override
+    public int getElevatorDoorStatus(int elevatorNumber) throws RemoteException {
+        return elevators.get(elevatorNumber).getDoorState();
+    }
+
+    @Override
+    public int getElevatorFloor(int elevatorNumber) throws RemoteException {
+        return (int)Math.round((double)elevators.get(elevatorNumber).getLocation() / (double)this.floorHeight);
+    }
+
+    @Override
+    public int getElevatorNum() throws RemoteException {
+        return elevators.size();
+    }
+
+    @Override
+    public int getElevatorPosition(int elevatorNumber) throws RemoteException {
+        return elevators.get(elevatorNumber).getLocation();
+    }
+
+    @Override
+    public int getElevatorSpeed(int elevatorNumber) throws RemoteException {
+        return elevators.get(elevatorNumber).getSpeed();
+    }
+
+    @Override
+    public int getElevatorWeight(int elevatorNumber) throws RemoteException {
+        return elevators.get(elevatorNumber).getWeight();
+    }
+
+    @Override
+    public int getElevatorCapacity(int elevatorNumber) throws RemoteException {
+        return elevators.get(elevatorNumber).getCapacity();
+    }
+
+    @Override
+    public boolean getFloorButtonDown(int floor) throws RemoteException {
+        return this.downButtonPress;
+    }
+
+    @Override
+    public boolean getFloorButtonUp(int floor) throws RemoteException {
+        return this.upButtonPress;
+    }
+
+    @Override
+    public int getFloorHeight() throws RemoteException {
+        return this.floorHeight;
+    }
+
+    @Override
+    public int getFloorNum() throws RemoteException {
+        return this.highestFloor - this.lowestFloor;
+    }
+
+    @Override
+    public boolean getServicesFloors(int elevatorNumber, int floor) throws RemoteException {
+        return elevators.get(elevatorNumber).getServiceableFloors().get(floor);
+
+    }
+
+    @Override
+    public int getTarget(int elevatorNumber) throws RemoteException {
+        return elevators.get(elevatorNumber).getTargetedFloor();
+    }
+
+    @Override
+    public void setCommittedDirection(int elevatorNumber, int direction) throws RemoteException {
+        elevators.get(elevatorNumber).setCommitedDirection(direction);
+    }
+
+    @Override
+    public void setServicesFloors(int elevatorNumber, int floor, boolean service) throws RemoteException {
+        elevators.get(elevatorNumber).setServiceableFloors(floor, service);
+    }
+
+    @Override
+    public void setTarget(int elevatorNumber, int target) throws RemoteException {
+        elevators.get(elevatorNumber).setTargetedFloor(target);
+    }
+
+    @Override
+    public long getClockTick() throws RemoteException {
+        return this.clockTick;
+    }
+
+    // update internal values, like speed, acceleration etc
+    // also update clock
+    private void updateAll()
+    {
+        for(int i = 0; i < elevators.size(); i++)
+        {
+            elevators.get(i).update();
+        }
+    }
+}
