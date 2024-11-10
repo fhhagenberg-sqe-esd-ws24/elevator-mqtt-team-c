e8a0b257c8af7e8e909df6a54a67fdcb2b9caf61
David Thomas Lee <lee.davidt@outlook.com>
2024-11-10 15:47:54 +0100

    Removed Example and zipped Javadoc

--- docu/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"at.fhhagenberg.sqelevator"},{"l":"at.wielander.elevator"},{"l":"at.wielander.elevator.controller"},{"l":"at.wielander.elevator.Model"},{"l":"at.wielander.elevator.View"}];updateSearchResults();
