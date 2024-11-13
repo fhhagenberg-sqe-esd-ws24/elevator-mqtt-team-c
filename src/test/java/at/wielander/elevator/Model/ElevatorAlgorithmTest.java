38c7661c8c84443d5fdc9376dc2d59bbfbf3ae80
David Thomas Lee <lee.davidt@outlook.com>
2024-11-13 20:50:59 +0100

    Added JaCoCO unit test runtime agent and report generator to pom.xml

---
+++ src/test/java/at/wielander/elevator/Model/ElevatorAlgorithmTest.java
@@ -0,0 +1,26 @@
+package at.wielander.elevator.Model;
+
+class ElevatorAlgorithmTest {
+/*
+    private ElevatorAlgorithm algorithm;
+
+    @BeforeEach
+    void setUp() {
+        algorithm = new ElevatorAlgorithm();
+    }
+
+    @Test
+    void testProcessCallRequest() {
+        // Testet, ob der Algorithmus eine Anforderung korrekt verarbeitet
+        int floor = 2;
+        algorithm.processCallRequest(floor);
+        assertEquals(floor, algorithm.getCurrentFloor());
+    }
+
+    @Test
+    void testHandleOverload() {
+        // Testet, ob die Überlastung korrekt behandelt wird
+        algorithm.setPayload(1500);
+        assertTrue(algorithm.isOverloaded());
+    }*/
+}
