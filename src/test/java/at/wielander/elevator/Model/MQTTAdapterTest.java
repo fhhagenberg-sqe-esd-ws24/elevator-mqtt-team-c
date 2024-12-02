06cf1f9b752aafc66b491c16ef5a0bd4c6b9a15c
David Thomas Lee <lee.davidt@outlook.com>
2024-12-02 14:03:07 +0100

    Added minor changes

--- src/test/java/at/wielander/elevator/Model/MQTTAdapterTest.java
+++ src/test/java/at/wielander/elevator/Model/MQTTAdapterTest.java
@@ -9,11 +9,9 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.*;
 
 
 @ExtendWith(MockitoExtension.class)
